191152a910a0d30030ab2eed3f305395ded78a30
biyasto <biyasto@gmail.com>
2021-05-28 12:19:49 +0700

    also Try to fix conflict

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Test Layout chỗ này
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("SearchScreen.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateAccount.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("8=>");
         primaryStage.setScene(new Scene(root, 300, 275));
