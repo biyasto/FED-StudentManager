4c817c20b24294c43f4372b839405b6838e33259
Johnny Luu <sangjohn58@gmail.com>
2021-06-13 02:35:13 +0700

    add database, complete login function

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -5,17 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Test Layout chỗ này
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/UserDetail.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/Login.fxml"));
         //
         Parent root = loader.load();
-        primaryStage.setTitle("8=>");
-        primaryStage.setScene(new Scene(root, 700, 450));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module QuanLyHocSInh {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.controls;
+    requires java.sql;
+    requires mysql.connector.java;
     opens GUI;
     opens GUI.resources;
     opens GUI.controllers;
