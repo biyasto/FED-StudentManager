0361d203bb402049481051d35a9537065fdb65b6
biyasto <biyasto@gmail.com>
2021-06-13 16:51:44 +0700

    Ad Navigation

---
+++ src/GUI/controllers/NavigationController.java
@@ -0,0 +1,73 @@
+package GUI.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class NavigationController {
+
+    @FXML
+    private Button SearchClassButton;
+
+    @FXML
+    private Button InfoButton;
+
+    @FXML
+    private Button ScheduleButton;
+
+    @FXML
+    private Button SearchUsersButton;
+
+    @FXML
+    private ImageView Avatar;
+
+    @FXML
+    private Label NameLabel;
+
+    @FXML
+    private Button CreateAccountButton;
+
+    @FXML
+    private Button SettingButton;
+
+    @FXML
+    private ImageView LogoutButton;
+
+    @FXML
+    void Logout(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenCreateAccounScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenInfomationScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenScheduleScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenSearchClassesScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenSearchUserScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OpenSettingScreen(MouseEvent event) {
+
+    }
+
+}
