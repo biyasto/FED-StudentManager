91b4cbbd5d4700d9ba66b95976d49c195b5fe441
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-06-15 16:39:32 +0700

    add some func to DAL

--- src/DataAccessLayer/ExamScheduleDAL.java
+++ src/DataAccessLayer/ExamScheduleDAL.java
@@ -3,5 +3,40 @@ package DataAccessLayer;
 import java.util.Date;
 
 public class ExamScheduleDAL {
+    private DatabaseUtils DBU = null;
+    private Connection conn = null;
+    private PreparedStatement pres = null;
+    private ResultSet rs = null;
 
+    public List<ExamScheduleDTO> getAllExamSchedule(){
+        List<ExamScheduleDTO> list = new ArrayList<>();
+        String sql = "select * from ExamSchedule";
+        try {
+            DBU = new DatabaseUtils();
+            conn = DBU.createConnection();
+            pres = conn.prepareStatement(sql);
+            rs = pres.executeQuery();
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+            while (rs.next()) {
+                ExamScheduleDTO s = new ExamScheduleDTO();
+                s.setClassId(rs.getString("classId"));
+                s.setExamDate(rs.getDate("examdate"));
+                s.setExamType(rs.getInt("flag"));
+
+                list.add(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+                pres.close();
+                rs.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return list;
+    }
 }
--- src/DataAccessLayer/SubjectClassDAL.java
+++ src/DataAccessLayer/SubjectClassDAL.java
@@ -1,4 +1,42 @@
 package DataAccessLayer;
 
 public class SubjectClassDAL {
+    private DatabaseUtils DBU = null;
+    private Connection conn = null;
+    private PreparedStatement pres = null;
+    private ResultSet rs = null;
+
+    public List<SubjectClassDTO> getAllSubjectClass(){
+        List<SubjectClassDTO> list = new ArrayList<>();
+        String sql = "select * from SubjectClass";
+        try {
+            DBU = new DatabaseUtils();
+            conn = DBU.createConnection();
+            pres = conn.prepareStatement(sql);
+            rs = pres.executeQuery();
+
+            while (rs.next()) {
+                SubjectClassDTO s = new SubjectClassDTO();
+                s.setClassId(rs.getString("classId"));
+                s.setHeadMaster(rs.getString("headMaster"));
+                s.setSubjectId(rs.getString("subjectId"));
+                s.setSchoolYear(rs.getInt("schoolYear"));
+                s.setSemester(rs.getInt("semester"));
+                s.setFaculty(rs.getString("faculty"));
+
+                list.add(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+                pres.close();
+                rs.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return list;
+    }
 }
