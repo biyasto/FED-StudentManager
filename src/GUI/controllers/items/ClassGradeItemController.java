e50e781e61ca5de92b39be6bc2545e35f5ed146c
biyasto <biyasto@gmail.com>
2021-06-16 22:48:59 +0700

    Event item

---
+++ src/GUI/controllers/items/ClassGradeItemController.java
@@ -0,0 +1,49 @@
+package GUI.controllers.items;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClassGradeItemController{
+
+    @FXML
+    private AnchorPane StudentInfoBox;
+
+    @FXML
+    private AnchorPane GradeBox;
+
+    @FXML
+    private Label Grade1Label;
+
+    @FXML
+    private TextField Grade1Textfield;
+
+    @FXML
+    private Label Grade2Label;
+
+    @FXML
+    private TextField Grade2Textfield;
+
+    @FXML
+    private Label Grade3Label;
+
+    @FXML
+    private TextField Grade3Textfield;
+
+    @FXML
+    private Label Grade4Label;
+
+    @FXML
+    private TextField Grade4Textfield;
+
+    @FXML
+    private Label SumGradeLabel;
+
+    @FXML
+    private Label StudentNam;
+
+    @FXML
+    private Label StudentID;
+
+}
