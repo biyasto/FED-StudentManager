6094870bb54fd61a7ac65eafadc3d1de23e6bbe8
Johnny Luu <sangjohn58@gmail.com>
2021-06-16 17:45:13 +0700

    complete search classes

--- src/GUI/controllers/SearchClassesController.java
+++ src/GUI/controllers/SearchClassesController.java
@@ -1,8 +1,10 @@
 package GUI.controllers;
 
+import BusinessLogicLayer.StudentBLL;
 import BusinessLogicLayer.SubjectBLL;
 import BusinessLogicLayer.SubjectClassBLL;
 import BusinessLogicLayer.TeacherBLL;
+import DataTransferObject.StudentDTO;
 import DataTransferObject.SubjectClassDTO;
 import DataTransferObject.SubjectDTO;
 import DataTransferObject.TeacherDTO;
@@ -12,7 +14,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.File;
@@ -34,6 +38,12 @@ public class SearchClassesController implements Initializable {
     @FXML
     private VBox classScrollPane;
 
+    @FXML
+    private Label lblNotFound;
+
+    @FXML
+    private Label lblEmpty;
+
     private List<SubjectClassDTO> classList = new ArrayList<>();
 
     @Override
@@ -41,18 +51,25 @@ public class SearchClassesController implements Initializable {
         SubjectClassBLL subjectClassBLL = new SubjectClassBLL();
         classList = subjectClassBLL.getAllSubjectClass();
 
-        for(SubjectClassDTO subjectClass: classList) {
-            SubjectBLL subjectBLL = new SubjectBLL();
-            TeacherBLL teacherBLL = new TeacherBLL();
+        //load all class into table
+        for(SubjectClassDTO subjectClass: classList)
+            loadDataIntoTable(subjectClass);
 
-            SubjectDTO subjectDTO = subjectBLL.GetSubjectById(subjectClass.getSubjectId());
-            TeacherDTO teacherDTO = teacherBLL.GetTeacherByID(subjectClass.getHeadMaster());
-
-            bindData(teacherDTO, subjectDTO, subjectClass);
-        }
     }
 
-    void bindData(TeacherDTO teacher, SubjectDTO subject, SubjectClassDTO subjectClass) {
+    void loadDataIntoTable(SubjectClassDTO subjectClass) {
+        SubjectBLL subjectBLL = new SubjectBLL();
+        TeacherBLL teacherBLL = new TeacherBLL();
+        StudentBLL studentBLL = new StudentBLL();
+
+        SubjectDTO subjectDTO = subjectBLL.GetSubjectById(subjectClass.getSubjectId());
+        TeacherDTO teacherDTO = teacherBLL.GetTeacherByID(subjectClass.getHeadMaster());
+        List<StudentDTO> studentList = studentBLL.getStudentsByClassId(subjectClass.getClassId());
+
+        bindData(studentList, teacherDTO, subjectDTO, subjectClass);
+    }
+
+    void bindData(List<StudentDTO> studentList, TeacherDTO teacher, SubjectDTO subject, SubjectClassDTO subjectClass) {
         try {
             URL urlLayout = new File("src/GUI/resources/items/ClassItem.fxml").toURI().toURL();
             FXMLLoader fxmlLoader = new FXMLLoader();
@@ -60,12 +77,63 @@ public class SearchClassesController implements Initializable {
             Node item = fxmlLoader.load();
 
             ClassItemController classItemController = fxmlLoader.getController();
-            classItemController.setData(teacher, subject, subjectClass);
+            classItemController.setData(studentList, teacher, subject, subjectClass);
 
             classScrollPane.getChildren().add(item);
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void findClass(MouseEvent event) {
+        String findID = ClassIDTextfield.getText();
+        String findName = SubjectNameTextfield.getText();
+        boolean isFound = false;
+
+        ClassIDTextfield.clear();
+        SubjectNameTextfield.clear();
+
+        if(findID.isEmpty() && findName.isEmpty()) {
+            lblNotFound.setVisible(false);
+            lblEmpty.setVisible(true);
+        }
+        else {
+            if(!findID.isEmpty()) {
+                classScrollPane.getChildren().clear();
+
+                for(SubjectClassDTO subjectClass: classList) {
+                    if(subjectClass.getClassId().equals(findID)) {
+                        loadDataIntoTable(subjectClass);
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            else {
+                classScrollPane.getChildren().clear();
+
+                for(SubjectClassDTO subjectClass: classList) {
+                    //find subject base on class
+                    SubjectBLL subjectBLL = new SubjectBLL();
+                    SubjectDTO subjectDTO = subjectBLL.GetSubjectById(subjectClass.getSubjectId());
+
+                    if(subjectDTO.getSubjectName().equals(findName)) {
+                        loadDataIntoTable(subjectClass);
+                        isFound = true;
+                    }
+                }
+            }
+
+            if(isFound) {
+                lblNotFound.setVisible(false);
+                lblEmpty.setVisible(false);
+            }
+            else {
+                lblNotFound.setVisible(true);
+                lblEmpty.setVisible(false);
+            }
         }
     }
 }
--- src/GUI/controllers/items/ClassItemController.java
+++ src/GUI/controllers/items/ClassItemController.java
@@ -1,11 +1,14 @@
 package GUI.controllers.items;
 
+import DataTransferObject.StudentDTO;
 import DataTransferObject.SubjectClassDTO;
 import DataTransferObject.SubjectDTO;
 import DataTransferObject.TeacherDTO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.List;
 
 public class ClassItemController {
     @FXML
@@ -29,19 +32,22 @@ public class ClassItemController {
     @FXML
     private Label AttendantsLabel;
 
+    private List<StudentDTO> studentList = null;
     private TeacherDTO teacher = null;
     private SubjectDTO subject = null;
     private SubjectClassDTO subjectClass = null;
 
-    public void setData(TeacherDTO teacher, SubjectDTO subject, SubjectClassDTO subjectClass) {
+    public void setData(List<StudentDTO> studentList, TeacherDTO teacher, SubjectDTO subject, SubjectClassDTO subjectClass) {
+        this.studentList = studentList;
         this.teacher = teacher;
         this.subject = subject;
         this.subjectClass = subjectClass;
 
         ClassIDLabel.setText(subjectClass.getClassId());
         SubjectLabel.setText(subject.getSubjectName());
-        FacultyLabel.setText(subject.getFaculty());
-        TeacherLabel.setText(teacher.getName());
+        FacultyLabel.setText("Faculty: " + subject.getFaculty());
+        TeacherLabel.setText("Teacher: " + teacher.getName());
+        AttendantsLabel.setText("Attendants: " + studentList.size());
     }
 
 }
